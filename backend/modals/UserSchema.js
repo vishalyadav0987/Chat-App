f52a12ebf6923f1fe00f964a0a9830f666526c2c
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-11 16:22:38 +0530

    Create User Schema

---
+++ backend/modals/UserSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    gender:{
+        type:String,
+        required:true,
+       enum:["male","female"],
+    },
+    profilePic:{
+        type:String,
+        default:"",
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
