36570443e04829a73ffe774bbed0582399788217
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-11 20:36:28 +0530

    Creating login logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -39,15 +39,32 @@ const Register = async (req, res) => {
         }
 
     } catch (error) {
-        // console.log("Erorr in auth controller register function: ", error.message)
+        console.log("Erorr in auth controller register function: ", error.message)
         res.status(500).json({ success: false, error: error.message, msg: "Something went wrong!" });
     }
 }
 const Login = async (req, res) => {
-    res.send("This login routr");
+    // res.send("This login routr");
+    try {
+        const { username, password } = req.body;
+        const user = await UserSchema.findOne({username});
+        const isPassword = await bcrypt.compare(password,user.password || '');
+
+        if(!user || !isPassword){
+            res.status(400).json({ success: false, msg: "Invalid Crendential!" });
+        }
+        generateToken(user._id,res);
+        const {password: userPassword,...resData} = user._doc;
+        res.status(200).json({ success: true, resData, msg: "User successfull logged in!" });
+
+    } catch (error) {
+        console.log("Erorr in auth controller login function: ", error.message);
+        res.status(500).json({ success: false, error: error.message, msg: "Something went wrong!" });
+    }
 }
 const Logout = async (req, res) => {
-    res.send("This logout routr");
+    // res.send("This logout routr");
+    
 }
 
 module.exports = {
