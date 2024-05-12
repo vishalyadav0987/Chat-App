dc0e6b8761a4bd384021b07c4a20550d6bcd5fec
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-12 11:33:32 +0530

    Setup the tailwindcss and daisyUi

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
