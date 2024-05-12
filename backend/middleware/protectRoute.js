84e7c0291914258e7e72958164ca50232d873565
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-12 08:34:13 +0530

    protect router middleware

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const UserSchema = require('../modals/UserSchema');
+
+const protectRouteMiddleware = async (req, res, next) => {
+    try {
+        const token = req.cookie.token;
+        if (!token) {
+            return res.status(401).json({ success: false, msg: "Unauhtorized - No token provided" });
+        }
+        const decode = jwt.verify(token, process.env.JWT_SECERET);
+        if (!decode) {
+            return res.status(401).json({ success: false, msg: "Unauhtorized - No token provided" });
+        }
+        console.log(decode);
+        const user = await UserSchema.findById(decode.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ success: false, msg: "User not found!" });
+        }
+    } catch (error) {
+        console.log("Error in protect route", error.message);
+
+        return res.status(401).json({ success: false, error: error.message, msg: "Something went wrong!" });
+    }
+}
