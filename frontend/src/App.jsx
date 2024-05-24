eaa71c3f8885b2ebe46c0f3a53f6d7869de003ee
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-24 11:02:40 +0530

    Set Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Home from './Pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Login/Login'
+import Register from './Register/Register'
+
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Hlo this chat App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="bg-slate-950 h-[100vh] overflow-hidden">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
