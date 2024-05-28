b6c095105c2d0cbd485252ca668c5bb8a1d2fcde
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-28 13:18:50 +0530

    Add logic sending message to one user to another using useSendMessage

---
+++ frontend/src/Hooks/useSendMessage.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import useConversation from "../Store/userConversation";
+import { toast } from 'react-hot-toast'
+
+
+const useSendMessage = () => {
+    const [loading, setLoading] = useState(false);
+    const { messages, setMessages, selectedConversation } = useConversation();
+    const sendMessage = async (message) => {
+        setLoading(true);
+        try {
+            const res = await fetch(`api/message/send/${selectedConversation._id}`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ message })
+            });
+            const data = await res.json();
+            if (res.ok) {
+                setMessages([...messages, data]);
+            }
+            else {
+                toast.error(data.msg);
+            }
+        } catch (error) {
+            toast.error(error.message);
+
+        } finally {
+            setLoading(false);
+        }
+    }
+    return { sendMessage, loading };
+}
+
+export default useSendMessage
