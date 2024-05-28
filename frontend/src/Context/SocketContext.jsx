8d34d4e4bed261ee723c9d1d971edeef28a0e978
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-28 16:55:58 +0530

    At frontend create  socket context  and fuctionality user online or offline in an object

---
+++ frontend/src/Context/SocketContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import io from 'socket.io-client';
+import { useAuthContext } from '../Context/AuthContext';
+
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext);
+}
+
+export const SocketContextProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null);
+    const [onlineUser, setOnlineUser] = useState([]);
+    const { authUser } = useAuthContext();
+
+    useEffect(() => {
+        if (authUser) {
+            const newSocket = io("http://localhost:5000", {
+                query: {
+                    userId: authUser.resData._id,
+                }
+            });
+
+            setSocket(newSocket);
+
+            newSocket.on("getOnlineUsers", (users) => {
+                setOnlineUser(users);
+                console.log(users);
+            });
+
+            return () => {
+                newSocket.close();
+                setSocket(null);
+            };
+        } else if (socket) {
+            socket.close();
+            setSocket(null);
+        }
+    }, [authUser]);
+
+    return (
+        <SocketContext.Provider value={{ socket, onlineUser }}>
+            {children}
+        </SocketContext.Provider>
+    );
+}
