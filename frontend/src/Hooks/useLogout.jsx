8f252b1aa0a9f89b2ad7875491b20f9fbdb8718e
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2024-05-28 13:13:30 +0530

    Add logout logic using jwt and create useLogout

---
+++ frontend/src/Hooks/useLogout.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { useAuthContext } from '../Context/AuthContext';
+import { toast } from 'react-hot-toast'
+
+const useLogout = () => {
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
+
+    const logout = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch('/api/auth/logout', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            })
+            const data = await res.json();
+            if (res.ok) {
+                // console.log(data);
+                localStorage.removeItem("chat-user");
+                toast.success("User Succesfully logged out");
+                setAuthUser(null)
+            }
+            else {
+                toast.error(data.msg);
+            }
+        } catch (error) {
+            toast.error(error.message);
+
+        } finally {
+            setLoading(false);
+        }
+    }
+    return { loading, logout }
+}
+
+export default useLogout
